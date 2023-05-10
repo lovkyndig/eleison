7c0b8980b0ac5d616e8deebd205a78a95e02e3ed
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-05-10 17:56:22 +0200

    v1.0.1-rc.4

--- dist/_nuxt/selectAll.97185faf.js
+++
@@ -1,1 +0,0 @@
-import{S as t,r as n,e as o}from"./transform.3f90e15a.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
