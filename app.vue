b8b7a1747e13f91fd9168fdcb075fd90f5ac6e3f
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-06-08 13:38:51 +0200

    v1.0.4-rc.21

--- app.vue
+++ app.vue
@@ -17,6 +17,14 @@ const gtag_header = `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', '${gtag_id}');`
+
+const gtm_header = `<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JN2RKR');</script>
+<!-- End Google Tag Manager -->`
 /**
  *
  * set head meta for all pages
@@ -29,6 +37,7 @@ useHead({
   htmlAttrs: { lang: 'no' },
   // body-script: https://github.com/nuxt/nuxt/issues/13069
   script: [
+    { children: gtm_header }
     // { src: gtag_src, async: true },
     // { innerHTML: gtag_header },
     // { src: 'header-script.js', defer: true }
