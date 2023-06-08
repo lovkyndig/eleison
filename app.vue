4803f17abe68a1b0db52331119f3023fee91556d
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-06-08 13:54:47 +0200

    v1.0.4-rc.22

--- app.vue
+++ app.vue
@@ -18,13 +18,11 @@ const gtag_header = `window.dataLayer = window.dataLayer || [];
   gtag('js', new Date());
   gtag('config', '${gtag_id}');`
 
-const gtm_header = `<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5JN2RKR');</script>
-<!-- End Google Tag Manager -->`
+const gtm_header = `(function(w,d,s,l,i){
+  w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+  var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+  j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JN2RKR');`
 /**
  *
  * set head meta for all pages
